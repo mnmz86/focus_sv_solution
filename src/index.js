f490267cd76a35fa9d033003335c8632b9d95322
Martín Muñoz <martin_eros@msn.com>
2019-11-17 02:00:36 -0600

    Al menos sigo aprendiendo, falta responsive design

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import PostContainer from './components/post-container.js';
 import UserInfo from './components/user-info.js';
-import {getUsersData, getPostsData, getAllData} from './services.js';
+import {getAllData} from './services.js';
 
-const rootTag = document.getElementById('root');
-getUsersData().then(users =>{
+let rootTag = document.getElementById('root');
+getAllData().then(users => {
     users.forEach(user => {
         let userTag = document.createElement('user-info');
         userTag.setAttribute('data-user', JSON.stringify(user));
         root.appendChild(userTag);
+        user.posts.forEach(post => {
+            let postTag = document.createElement('post-container');
+            postTag.setAttribute('data-post', JSON.stringify(post));
+            user.posts.indexOf(post)%2 ? postTag.setAttribute('class', 'red') : null
+            userTag.appendChild(postTag);
+        });
     });
 });
-
-const userTags = document.getElementsByTagName('user-info');
-getPostsData().then(posts => {
-    posts.forEach(post => {
-        let postTag = document.createElement('post-container');
-        postTag.setAttribute('data-post', JSON.stringify(post));
-        posts.indexOf(post)%2 ? postTag.setAttribute('class', 'red') : 0
-        userTags[post.userId-1].appendChild(postTag);
-    });
-});
-
-getAllData().then(result => console.log(result))
