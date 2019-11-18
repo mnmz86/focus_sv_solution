cc4577049c4538f3e959c5de0f3d351dd90f7c6b
Martín Muñoz <martin_eros@msn.com>
2019-11-18 10:40:43 -0600

    Ya por el apendizaje, resolví el problema del sexo

--- src/components/user-info.js
+++ src/components/user-info.js
@@ -12,9 +12,9 @@ export default class UserInfo extends HTMLElement{
     }
 
     render() {
-        const imgUrl = `//joeschmoe.io/api/v1/${this.user.firstName}`;
+        const imgUrl = `//joeschmoe.io/api/v1/${this.user.gender}/${this.user.firstName}`;
         const userTemplate = `
-        <img src="${imgUrl}" alt="Avatar">
+        <img src="${imgUrl}" alt="${this.user.firstName}">
         <h2>${this.user.name}</h2>
         `;
         let userNode = document.createElement('div');
--- src/services.js
+++ src/services.js
@@ -1,18 +1,42 @@
 export async function getUsersData(){
-  let response = await fetch('//jsonplaceholder.typicode.com/users');
+  const response = await fetch('//jsonplaceholder.typicode.com/users');
   let data = await  response.json();
-  let dataWithFirstNames = data.map(
-    user => {
-      if((user.name.split(' ')[0]).indexOf('.') == -1) {
-        user.firstName = user.name.split(' ')[0]
+  let usersFirstName = [];
+  //extracción de primer nombre
+  data.forEach(
+      user => {
+        //manejo de prefijo abreviado Mrs.
+        if((user.name.split(' ')[0]).indexOf('.') == -1) {
+          user.firstName = user.name.split(' ')[0]
+        }
+        else {
+          user.firstName = user.name.split(' ')[1]
+        }
+        usersFirstName.push(user.firstName);
+        return user;
       }
-      else {
-        user.firstName = user.name.split(' ')[1]
-      }
-      return user;
-    }
+    );
+
+  //emular el navegador y evitar la el bloqueo desde otro origen
+  const stringNamesParam = 'name[]='.concat(usersFirstName.join('&name[]='));
+  const genderUrl = `https://api.genderize.io/?${stringNamesParam}`;
+  const scrapPayload = {
+    url: genderUrl,
+    renderType: "plainText",
+    outputAsJson: true,
+    suppressJson:["originalRequest","pageResponses","meta.trace", "queryJson"]
+  };
+  const scrapUrl = `//PhantomJsCloud.com/api/browser/v2/ak-1jwav-bg6ng-92gm3-atm3c-mnsv2/?requestBase64=${btoa(JSON.stringify(scrapPayload))}`
+  const gendersRequest = await fetch(scrapUrl);
+  const gendersResponse = await gendersRequest.json();
+  let genders = JSON.parse(gendersResponse.content.data);
+  
+  //agregado de género
+  data.forEach(
+    (user, i) => user.gender = genders[i].gender
   );
-  return dataWithFirstNames;
+
+  return data;
 }
 
 export async function getPostsData() {
